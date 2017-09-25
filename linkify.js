93036d269834241c5f765b6218db9a984d41b07b
Coleman <crollins199@gmail.com>
2017-09-25 12:04:53 -0500

    [cr] default options and prettier-ized

--- linkify.js
+++ linkify.js
@@ -1,37 +1,38 @@
-
 // Links have the notation: [[<link text>, <link target>]]
 // linkify takes in an element containing text with a link
 // and replaces it with a built <a> tag
-const linkify = (el, options = {}) => {
-    // Get the text from the element and isolate the link string, also split
-    // into an array.
-    let startingText = el.innerText;
-    let reg = startingText.match(/\[\[(.*?)\]\]/g).map(function(v){return v.replace(/\[+|\]+/g,'');});
-    let count = 0;
-    for (let i of reg) {
-      let arr = i.split(',');
-      let link = document.createElement('a');
-      link.setAttribute('href', arr[1]);
-      link.innerHTML = arr[0];
-      if (options.targetBlank) {
-        link.setAttribute('target', '_blank');
-      }
-      if (options.class.length > 0) {
-        let classes = '';
-        for (let i of options.class) {
-          classes = classes + i + ' ';
-        }
-        link.setAttribute('class', classes);
-      }
-      let replaceText = `[[${i}]]`;
-      if (count === 0) {
-        let newText = startingText.replace(replaceText, link.outerHTML);
-        el.innerHTML = newText;
-      } else {
-        let midwayText = el.innerHTML;
-        let newText = midwayText.replace(replaceText, link.outerHTML);
-        el.innerHTML = newText;
-      }  
-      count++;
+const linkify = (el, { targetBlank = false, classes = [] }) => {
+  // Get the text from the element and isolate the link string, also split
+  // into an array.
+
+  let startingText = el.innerText;
+  let reg = startingText.match(/\[\[(.*?)\]\]/g).map(function(v) {
+    return v.replace(/\[+|\]+/g, "");
+  });
+  let count = 0;
+  for (let i of reg) {
+    let arr = i.split(",");
+    let link = document.createElement("a");
+    link.setAttribute("href", arr[1]);
+    link.innerHTML = arr[0];
+    if (targetBlank) {
+      link.setAttribute("target", "_blank");
     }
-}
+    if (classNames.length === 0) {
+      classNames = [];
+    } else {
+      link.classList.add(...classNames);
+    }
+
+    let replaceText = `[[${i}]]`;
+    if (count === 0) {
+      let newText = startingText.replace(replaceText, link.outerHTML);
+      el.innerHTML = newText;
+    } else {
+      let midwayText = el.innerHTML;
+      let newText = midwayText.replace(replaceText, link.outerHTML);
+      el.innerHTML = newText;
+    }
+    count++;
+  }
+};
