663f42026403e0a2291d6d1263ad56ec7e12085c
Coleman <crollins199@gmail.com>
2017-09-25 11:30:46 -0500

    [cr] working linkify with a DOM element

---
+++ linkify.js
@@ -0,0 +1,28 @@
+
+// Links have the notation: [[<link text>, <link target>]]
+// linkify takes in an element containing text with a link
+// and replaces it with a built <a> tag
+const linkify = (el) => {
+    // Get the text from the element and isolate the link string, also split
+    // into an array.
+    let startingText = el.innerText;
+    let reg = startingText.match(/\[\[(.*?)\]\]/g).map(function(v){return v.replace(/\[+|\]+/g,'');});
+     
+    let count = 0;
+    for (let i of reg) {
+      let arr = i.split(',');
+      let link = document.createElement('a');
+      link.setAttribute('href', arr[1]);
+      link.innerHTML = arr[0];
+      let replaceText = `[[${i}]]`;
+      if (count === 0) {
+        let newText = startingText.replace(replaceText, link.outerHTML);
+        el.innerHTML = newText;
+      } else {
+        let midwayText = el.innerHTML;
+        let newText = midwayText.replace(replaceText, link.outerHTML);
+        el.innerHTML = newText;
+      }  
+      count++;
+    }
+}
