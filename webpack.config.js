fe0a12e35153aaf888c19b466670ba0b38f8720d
Coleman <crollins199@gmail.com>
2017-09-25 14:14:22 -0500

    [cr] using webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const webpack = require("webpack");
+const glob = require("glob");
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+// Options for plugins
+let cleanPaths = ["./src/*"];
+let cleanOptions = {
+  verbose: true,
+  watch: true
+};
+
+// Config
+const config = {
+  context: path.resolve(__dirname),
+  target: "node",
+  entry: {
+    linkify: "./index.js"
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: "./src/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [["es2015", { modules: false }]]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // Simple progress bar
+    new ProgressBarPlugin(),
+    // Delete all old stuff on compile
+    new CleanWebpackPlugin(cleanPaths, cleanOptions),
+    // Something something scope hoisting webpack 3
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    //new BundleAnalyzerPlugin(),
+    new webpack.DefinePlugin({
+      global: {}, // bizarre lodash(?) webpack workaround
+      "global.GENTLY": false // superagent client fix
+    })
+  ]
+};
+
+process.noDeprecation = true;
+module.exports = config;
